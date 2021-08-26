493e594a1de31951c2131076a15655747288bc82
n-y-kim <nyn2265@gmail.com>
2021-08-27 02:21:07 +0900

    first try with ml model

---
+++ ScoreTestApp/ScoreTestApp/ResultVC.swift
@@ -0,0 +1,31 @@
+//
+//  ResultVC.swift
+//  test-score
+//
+//  Created by Nayeon Kim on 2021/08/26.
+//
+
+import UIKit
+
+class ResultVC: UIViewController {
+
+    @IBOutlet weak var resultScore: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
